30c1f82d49c2687b674ac13066e1d10114c9f866
sejaldutta <sejaldutta2044@gmail.com>
2026-04-25 02:17:15 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_URL || '/minor-project',
+  base: process.env.VITE_BASE_PATH || '/minor-project',
 })
 
